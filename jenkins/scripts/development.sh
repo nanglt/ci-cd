c5c354d98fbadccacc36668785854724291c48c0
nanglt <148749897+nanglt@users.noreply.github.com>
2024-01-30 11:49:55 +0700

    Update development.sh

--- jenkins/scripts/development.sh
+++ jenkins/scripts/development.sh
@@ -1,4 +1,4 @@
-echo 'Delivering for development...f'
+echo 'Delivering for development...fr'
 
 # Use built docker image from step 'Package'
 # Docker image from step Package still pushed to Google container registry.
